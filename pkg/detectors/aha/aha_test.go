7f032e524757d975921d864aeceefadd2d707ac1
Satthvik <77009368+Satthvik@users.noreply.github.com>
2022-06-06 09:37:36 +0530

    Update aha_test.go

--- pkg/detectors/aha/aha_test.go
+++ pkg/detectors/aha/aha_test.go
@@ -109,6 +109,7 @@ func BenchmarkFromData(benchmark *testing.B) {
 				_, err := s.FromData(ctx, false, data)
 				if err != nil {
 					b.Fatal(err)
+			Secret_key= ASKAFJAIBJPDNFS
 				}
 			}
 		})
